0fff95b670466e6131cb8d6e2655bfc0b9f08613
Adityaa050 <150373739+Adityaa050@users.noreply.github.com>
2025-07-14 15:59:45 +0530

    Time: 25 ms (5.34%), Space: 18.1 MB (19.55%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -1,16 +1,12 @@
-class Solution(object):
-    def isPalindrome(self, x):
-        i = x
-        re = 0
-        """
-        :type x: int
-        :rtype: bool
-        """
-        while x > 0:
-            r = x % 10
-            re = (re * 10) + r
-            x = x // 10
-        if i == re:
-            return True
-        else:
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
             return False
+        
+        rev = 0
+        num = x
+
+        while num != 0:
+            rev = rev * 10 + num % 10
+            num = num // 10
+        return rev == x
