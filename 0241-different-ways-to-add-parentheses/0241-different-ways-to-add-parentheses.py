b07a5cdb36a2d2bd6f32f3d672a0e9aa6941cdbc
Adityaa050 <150373739+Adityaa050@users.noreply.github.com>
2025-07-19 22:31:42 +0530

    Time: 4 ms (22.04%), Space: 17.7 MB (84.45%) - LeetHub

--- 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.py
+++ 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.py
@@ -1,41 +1,23 @@
 class Solution:
     def diffWaysToCompute(self, expression: str) -> List[int]:
-        # Memoization to store results of sub-expressions
-        memo = {}
-
-        # Helper function to compute the result recursively
-        def compute(expr):
-            # If the expression has already been computed, return the cached result
-            if expr in memo:
-                return memo[expr]
-            
-            # Result list for this sub-expression
-            res = []
-            
-            # Iterate through the expression
-            for i in range(len(expr)):
-                if expr[i] in '+-*':  # If the character is an operator
-                    # Split the expression into two parts and recursively compute their results
-                    left = compute(expr[:i])
-                    right = compute(expr[i+1:])
-                    
-                    # Apply the operator to every combination of results from left and right parts
-                    for l in left:
-                        for r in right:
-                            if expr[i] == '+':
-                                res.append(l + r)
-                            elif expr[i] == '-':
-                                res.append(l - r)
-                            elif expr[i] == '*':
-                                res.append(l * r)
-            
-            # If no operator is found, it means the expression is a single number
-            if not res:
-                res.append(int(expr))
-            
-            # Cache the result for this sub-expression
-            memo[expr] = res
-            return res
-        
-        # Call the helper function on the full expression
-        return compute(expression)
+        res = []
+        # ans = []
+        for i in range(len(expression)):
+            oper = expression[i]
+            if oper == "+" or oper == "-" or oper == "*":
+                sub_str1 = expression[0 : i]
+                sub_str2 = expression[i + 1 : ]
+                s1 = self.diffWaysToCompute(sub_str1)
+                s2 = self.diffWaysToCompute(sub_str2)
+                for i in s1:
+                    for j in s2:
+                        if oper == "*":
+                            res.append(int(i) * int(j))
+                        if oper == "+":
+                            res.append(int(i) + int(j))
+                        if oper == "-":
+                            res.append(int(i) - int(j))
+        if len(res) == 0:
+            res.append(int(expression))
+        # print(res)
+        return res
