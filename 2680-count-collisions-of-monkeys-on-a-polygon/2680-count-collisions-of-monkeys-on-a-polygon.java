f33ffe2c626e5c6b9edf6585fa3f8a680b340caa
Adityaa050 <150373739+Adityaa050@users.noreply.github.com>
2025-07-25 22:17:45 +0530

    Time: 0 ms (100%), Space: 40.4 MB (71.15%) - LeetHub

---
+++ 2680-count-collisions-of-monkeys-on-a-polygon/2680-count-collisions-of-monkeys-on-a-polygon.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int monkeyMove(int n) {
+        int mod = 1000000007;
+        int ans = (int) power(2, n, mod) - 2;
+        // In any case if ans is -ve, add the mod value to it
+        return ans + ((ans < 0)? mod : 0);
+    }
+    
+    private long power(long x, int y, int mod) {
+        long res = 1;
+        x %= mod; 
+        if (x == 0) {
+            return 0;
+        } 
+        while (y > 0) {
+            if ((y & 1) == 1) {
+                res = (res * x) % mod;
+            }
+            y >>= 1; 
+            x = (x * x) % mod;
+        }
+        
+        return res;
+    }
+}
