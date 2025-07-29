7d2c227d6e26418e0a0676a7852ac382aa041be0
Adityaa050 <150373739+Adityaa050@users.noreply.github.com>
2025-07-29 22:50:39 +0530

    Time: 15 ms (97.8%), Space: 35 MB (15.95%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
@@ -4,4 +4,5 @@ class Solution:
         for num in nums:
             if num in seen:
                 return num
-            seen.add(num)        
+            seen.add(num)
+        return -1       
