7572b0f1f912d51ad2aa02deeb6acacc4bfcd3e6
Adityaa050 <150373739+Adityaa050@users.noreply.github.com>
2025-08-02 14:40:38 +0530

    Time: 6 ms (84.89%), Space: 18.6 MB (35.01%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -3,8 +3,6 @@ class Solution:
         nums = "".join(re.findall(r'[a-zA-Z0-9]', s)).lower()
         if len(nums) == 1:
             return True
-        elif len(nums) == 2:
-            return nums[0] == nums[1]
         elif nums == nums[::-1]:
             return True
         else:
